7fbd4b978c4dadce647f3d9cdb5eb9cfe066a7f7
ryfoa6 <duckduckgoose384@gmail.com>
2021-05-22 15:29:12 -0700

    socials open in new tab

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,10 +7,10 @@ const Contact = () => (
 		<h3 class='role text-center'>Developer</h3>
 		<h3 class='name text-center m-1'>Ryan Foley</h3>
 		<div class='socials d-flex justify-content-center'>
-			<a href='https://github.com/ryfoa6'>
+			<a href='https://github.com/ryfoa6' target='_blank'>
 				<ion-icon name='logo-github' class='m-1'></ion-icon>
 			</a>
-			<a href='https://www.linkedin.com/in/ryfoa6/'>
+			<a href='https://www.linkedin.com/in/ryfoa6' target='_blank'>
 				<ion-icon name='logo-linkedin' class='m-1'></ion-icon>
 			</a>
 			<a href='mailto:ryfo720@gmail.com'>
