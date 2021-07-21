086e235451cff5d2df67dee1c83a3319de06df35
ryfoa6 <ryanmfoley84@gmail.com>
2021-07-21 13:04:08 -0700

    Update about me and nav-link font-size

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,9 +13,9 @@ const About = () => (
 				I design and build full stack applications using technologies such as
 				JavaScript, React, Node.js, Express, Python, Django, MongoDB, and
 				PostgreSQL. I'm a curious programmer who loves to reverse-engineer code
-				to develop a deeper understanding on how and why it works. I'm looking
-				for a company that will utilize my strong-problem solving, analytical,
-				and creative thinking skill-set.
+				to develop a deeper understanding on how and why it works. I'm seeking a
+				successful career with a company that will utilize my strong-problem
+				solving, analytical, and creative thinking skill-set.
 			</h4>
 		</div>
 	</div>
